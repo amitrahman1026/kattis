69c027808d8db7819125a3bd05ddbe27bca08784
amitrahman1026 <amit.amit.rahman@gmail.com>
2022-03-28 22:01:01 +0800

    Update knigsoftheforest.cpp

--- knigsoftheforest.cpp
+++ knigsoftheforest.cpp
@@ -1,71 +1,51 @@
-/* Kattis - Knigsoftheforest
-A relatively basic question on the use of priority queue. Just beware of the edge case where karl joins in 2011
-and you should be good to go :)
-Time: O((n+k) log (n+k)) for the sorting by year, the actual processing is O(n log k) due to n years, log k insert into pq
-Mem: O(n+k)
-*/
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <utility>
 #include <queue>
 
 using namespace std;
 
-int n, k;
-vector<pair<int,int>> contenders; // (year, strength)
-priority_queue<pair<int, bool>> pq; // (strength, is karl?)
-
-int main(){
+int main()
+{
+    int k, n;
     cin >> k >> n;
-    pair<int, int> karl;
-    int year, strength;
+    // karl
+    priority_queue<pair<int, int>> pq;
+    int y, p, karl_p;
+    cin >> y >> p;
+    karl_p = p;
 
-    cin >> year >> strength;
-    year -= 2011;
-    karl = make_pair(year, strength);
+    vector<pair<int, int>> v;
+    // push karl into vector
+    v.push_back({y,p});
 
-    bool karl_in = false;
-    if (karl.first == 0){
-        pq.push(make_pair(karl.second, true));
-        karl_in = true;
+    for (int i = 0; i < (n + k - 2); i++)
+    {
+        cin >> y >> p;
+        v.push_back({y, p});
+    }
+    // sort by contest year;
+    sort(v.begin(), v.end());
+    // take in k contestents
+    for (int i = 0; i < k; i++)
+    {
+        pq.push({v[i].second, v[i].first});
     }
 
-    for (int i=0;i<(n+k-2);i++){
-        cin >> year >> strength;
-        contenders.push_back(make_pair(year-2011,strength));
-    }
-    sort(contenders.begin(), contenders.end());
-    
-    int index=0;
-    for(;contenders[index].first==0; index++){
-        //printf("Pushing moose with strength: %d\n", contenders[index].second);
-        pq.push(make_pair(contenders[index].second, false));
-    }
-
-    bool won = false;
-    for(int i=0;i<n;i++){ // year i
-        pair<int,int> winner = pq.top();
+    for (int i = 0; i < n && !pq.empty(); i++)
+    {
+        pair<int, int> winner = pq.top();
         pq.pop();
-
-        if (winner.second){
-            cout << i+2011 << endl;
-            won = true;
-            break;
+        if (winner.first == karl_p){
+            cout << i+2011;
+            return 0;
         }
+        pq.push({v[k+i].second,v[k+i].first});
 
-        //printf("i: %d\n", i);
-
-        if (i+1 == karl.first && !karl_in){
-            //printf("Pushing karl\n");
-            pq.push(make_pair(karl.second, true));
-            karl_in = true;
-        }
-        else if (i!=n-1){
-            //printf("Pushing moose with strength: %d\n", contenders[index].second);
-            pq.push(make_pair(contenders[index].second, false));
-            index++;
-        }
     }
-    if (!won)cout << "unknown\n";
+
+    cout << "unknown\n";
 
     return 0;
 }
